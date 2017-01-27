518b7ab789ad12c68bdbc73fac248e4bbd46503e
SeregaSRG <monohrom98@gmail.com>
2017-01-27 10:25:27 +0300

    -

--- config.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('MYSQL_HOSTNAME','mysql.hostinger.ru');
-define('MYSQL_USERNAME','u893425600_trans');
-define('MYSQL_PASSWORD','90iDSkzQk5');
-define('MYSQL_DBNAME','u893425600_trans');
-define('MYSQL_CHARSET','utf8');
-define('SALT','USHv87h08jkdnlv2dhso*(w32rds');
